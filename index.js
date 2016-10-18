f98ac04cf74f848f21bd330374db8b8e61a69323
Kevin Parrott <parrott.kevin@gmail.com>
2016-10-18 00:46:36 -0400

    Not actually a webpage, just an api.

--- index.js
+++ index.js
@@ -1,85 +1,55 @@
-class World {
-  constructor () {
-    this.boardWidth = 500
-    this.boardHeight = 500
-    this.boardPadding = 10
-    const canvasWidth = this.boardWidth + (this.boardPadding * 2)
-    const canvasHeight = this.boardHeight + (this.boardPadding * 2)
-    const canvas = document.getElementById('world')
-    canvas.setAttribute('width', canvasWidth)
-    canvas.setAttribute('height', canvasHeight)
-    this.ctx = canvas.getContext('2d')
-  }
-
-  drawGrid () {
-    for (let x = 0; x <= this.boardWidth; x += 50) {
-      this.ctx.moveTo(0.5 + x + this.boardPadding, this.boardPadding)
-      this.ctx.lineTo(0.5 + x + this.boardPadding, this.boardHeight + this.boardPadding)
-    }
-
-    for (let y = 0; y <= this.boardHeight; y += 50) {
-      this.ctx.moveTo(this.boardPadding, 0.5 + y + this.boardPadding)
-      this.ctx.lineTo(this.boardWidth + this.boardPadding, 0.5 + y + this.boardPadding)
-    }
-    this.ctx.strokeStyle = 'black'
-    this.ctx.stroke()
-  }
-
-  renderRover (x, y, facing) {
-    const originX = Math.floor(x / 50) * 50 + 10
-    const originY = Math.floor(y / 50) * 50 + 10
-    const points = this.computePoints(originX, originY, facing)
-    this.ctx.beginPath()
-    this.ctx.moveTo(points.p0[0], points.p0[1])
-    this.ctx.lineTo(points.p1[0], points.p1[1])
-    this.ctx.lineTo(points.p2[0], points.p2[1])
-    this.ctx.fill()
-  }
-
-  // always start from the NW corner of the square
-  computePoints (x, y, facing) {
-    switch (facing) {
-      case 'n':
-        return {
-          p0: [x + 50, y + 50],
-          p1: [x, y + 50],
-          p2: [x + 25, y]
-        }
-      case 'e':
-        return {
-          p0: [x, y],
-          p1: [x, y + 50],
-          p2: [x + 50, y + 25]
-        }
-      case 's':
-        return {
-          p0: [x, y],
-          p1: [x + 50, y],
-          p2: [x + 25, y + 50]
-        }
-      case 'w':
-        return {
-          p0: [x + 50, y],
-          p1: [x + 50, y + 50],
-          p2: [x, y + 25]
-        }
+const translate = (x, y, facing, direction) => {
+  if (facing === 'n' || facing === 's') {
+    const obj = {x: x, facing: facing}
+    switch (direction) {
+      case 'f':
+        obj.y = facing === 'n' ? y + 1 : y - 1
+        return obj
+      case 'b':
+        obj.y = facing === 'n' ? y - 1 : y + 1
+        return obj
       default:
-        return
+        return obj
+    }
+  } else if (facing === 'e' || facing === 'w') {
+    const obj = {y: y, facing: facing}
+    switch (direction) {
+      case 'f':
+        obj.x = facing === 'e' ? x + 1 : x - 1
+        return obj
+      case 'b':
+        obj.x = facing === 'e' ? x - 1 : x + 1
+        return obj
+      default:
+        return obj
+    }
+  } else {
+    return {
+      x: x,
+      y: y,
+      facing: facing
     }
   }
-
 }
 
-const ready = function (cb) {
-  if (document.readyState !== 'loading') {
-    cb()
-  } else {
-    document.addEventListener('DOMContentLoaded', cb)
-  }
+const index = (init, moves) => {
+  let coords = [init]
+  moves.forEach((move, index) => {
+    if (index === 0) {
+      coords.push(translate(init.x, init.y, init.facing, move))
+    } else {
+      const obj = {
+        x: coords[index].x,
+        y: coords[index].y,
+        facing: coords[index].facing
+      }
+      coords.push(translate(obj.x, obj.y, obj.facing, move))
+    }
+  })
+  return coords
 }
 
-ready(() => {
-  const Start = new World()
-  Start.drawGrid()
-  Start.renderRover(65, 25, 's')
-})
+const init = {x: 0, y: 0, facing: 'e'}
+const moves = ['f', 'f', 'b']
+
+console.log(index(init, moves))
